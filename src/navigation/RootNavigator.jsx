88c14b0028556f580bbe9c4cc62cd6f5577d26c7
brooklyn-lyndontonga <brooklynlt24@gmail.com>
2025-11-11 11:19:35 +1300

    hub stacks and screens connected.

--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import React from "react"
 import { View, ActivityIndicator, Text } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
