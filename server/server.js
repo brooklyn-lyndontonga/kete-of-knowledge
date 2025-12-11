b7ac01bc6c2f80f98e7c0aece730289dba53d95e
brooklyn-lyndontonga <brooklynlt24@gmail.com>
2025-12-12 12:09:09 +1300

    updating expertnal url for admin and server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,8 @@
 /* eslint-disable no-undef */
 // server/server.js
 import app from "./app.js"
-import cors from "cors"
 
-// ----- CORS (must be BEFORE listen) -----
-app.use(
-  cors({
-    origin: "*", // temporary for development and deployment
-  })
-)
-
-// ----- SERVER -----
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`)
